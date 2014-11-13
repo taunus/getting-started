7b15ea6ba668cc406ebd63ee1a36c013c28e5789
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-11-12 21:10:34 -0300

    json

--- client/js/controllers/home/index.js
+++ client/js/controllers/home/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function (model, container, route) {
-  console.log('Rendered view %s using model %s', route.action, model);
+  console.log('Rendered view %s using model:\n%s', route.action, JSON.stringify(model, null, 2));
 };
